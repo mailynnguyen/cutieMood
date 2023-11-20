91a9be649c6e36f15d53826d251d25f2f32763bb
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2023-11-19 20:49:14 -0800

    add files and folders to dev

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import MoodCheckInScreen from './screens/MoodCheckInScreen';
+import CalendarScreen from './screens/CalendarScreen';
+import { useFonts } from 'expo-font';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [fontsLoaded] = useFonts({
+        'Gaegu-Reg': require('./assets/font/Gaegu-Regular.ttf'),
+        'Gaegu-Bold': require('./assets/font/Gaegu-Bold.ttf')
+    })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    if (!fontsLoaded) {
+        return <View><Text>Loading...</Text></View>;
+    }
+
+   
+
+    return (
+        
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerShown: false,
+                    }}
+                >
+                    <Stack.Screen
+                        name='MoodCheckIn'
+                        component={MoodCheckInScreen}
+                    />
+
+                    <Stack.Screen
+                        name='Calendar'
+                        component={CalendarScreen}
+                    />
+
+                </Stack.Navigator>
+
+            </NavigationContainer>
+
+        
+
+    )
+
+}
